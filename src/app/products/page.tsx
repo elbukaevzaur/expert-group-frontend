b96ee329bfe2a9bbd807dfec4d1c63e385b464d2
Zaur Elbukaev <elbukaevzaur@gmail.com>
2024-12-01 23:35:31 +0300

    Update page.tsx

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -8,7 +8,7 @@ export default function Products() {
     return (
         <div className="products">
             <div className="products__info_wrapper">
-                <h1 className="products__title">КАРНИЗЫ ПОТОЛОЧНЫЕ</h1>
+                <h1 className="products__title">КАРНИЗЫ ПОТОЛОЧНЫЕ test</h1>
                 <div className="products__info">
                     <h3 className="products__info_text">910</h3>
                     </div>
