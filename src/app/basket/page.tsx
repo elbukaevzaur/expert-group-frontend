ad62a2e66ace427f71f72313f9121ffa9bbe48a9
Khamadov <121062316+Khamadov@users.noreply.github.com>
2024-12-03 18:34:26 +0300

    first commit

---
+++ src/app/basket/page.tsx
@@ -0,0 +1,11 @@
+export default function Basket() {
+
+    return (
+        <div className="basket"> 
+            <div className="basket__title">
+                <h1 className="basket__title_text">Корзина</h1>
+                <p className="basket__subtitle">/ 3 шт.</p>
+            </div>
+        </div>
+    )
+}
