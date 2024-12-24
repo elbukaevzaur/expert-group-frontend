6ec9d95b5c9aad26264e200ee1692af88e6dec12
Khamadov <121062316+Khamadov@users.noreply.github.com>
2024-12-24 18:02:54 +0300

    корзина карточки детали

--- src/components/catalog/products-list-item-component.tsx
+++ src/components/catalog/products-list-item-component.tsx
@@ -5,6 +5,7 @@ import { EventHandler } from "react";
 import {useAppDispatch} from "@/lib/hooks";
 import {CHANGE_FAVORITES_REQUEST} from "@/lib/reducers";
 import {LikeSvg, MinusSmall, PlusSmall} from "@/lib/icon-svg";
+import styles from "./products-list-item-component.module.css"
 
 interface ProductsProps {
     key: number,
@@ -39,35 +40,35 @@ export function ProductsListItemComponent(props: ProductsProps) {
     }
 
     return (
-        <div className="item">
+        <div className={styles.item}>
             <Link href={`${getCustomLink()}`}>
-                <Image className="item__image" src={'/images/image.png'} alt="Карниз" width={295} height={149} />
+                <Image className={styles.image} src={'/images/image.png'} alt="Карниз" width={295} height={149} />
             </Link>
-            <div className="item__info">
-                <h3 className="item__name">{product.name}</h3>
-                <div className="item__wrraper">
-                    <div className="item__conteiner">
-                        <h3 className="item__text">{product.currentQuantity > 0 ? 'В наличии' : 'Нет в наличии'}</h3>
-                        <h2 className="item__price">{product.price} &#8381;</h2>
+            <div className={styles.info}>
+                <h3 className={styles.name}>{product.name}</h3>
+                <div className={styles.wrraper}>
+                    <div className={styles.conteiner}>
+                        <h3 className={styles.text}>{product.currentQuantity > 0 ? 'В наличии' : 'Нет в наличии'}</h3>
+                        <h2 className={styles.price}>{product.price} &#8381;</h2>
                     </div>
-                    <div className="item__action">
-                        <button onClick={handleChangeFavorite} className="item__like">
+                    <div className={styles.action}>
+                        <button onClick={handleChangeFavorite} className={styles.like}>
                             {
                                 <LikeSvg width={28} height={24} fill={isFavorite ? '#21A038' : 'none'}/>
                             }
                         </button>
                         {
                             props.basketItem == null ?
-                                <button className="item__basket" onClick={props.addToBasket}>
+                                <button className={styles.basket} onClick={props.addToBasket}>
                                     <Image src={'/images/Basket_white.png'} alt="Корзина" width={28} height={26}/>
-                                    <h3 className="item__basket_text">В корзину</h3>
+                                    <h3 className={styles.basket_text}>В корзину</h3>
                                 </button>
                                 :
-                                <div className="item__basket item__basket_cursor">
-                                    <div className="item__wrapper">
-                                    <button className="item__basket_button" onClick={props.removeFromBasket}>{<MinusSmall/>}</button>
-                                    <h3 className="item__basket_text item__basket_text_margin">{props.basketItem.quantity}</h3>
-                                    <button className="item__basket_button" onClick={props.addToBasket}>{<PlusSmall/>}</button>
+                                <div className={`${styles.basket} ${styles.basket_cursor}`}>
+                                    <div className={styles.wrapper}>
+                                    <button className={styles.basket_button} onClick={props.removeFromBasket}>{<MinusSmall/>}</button>
+                                    <h3 className={`${styles.basket_text} ${styles.basket_text_margin}`}>{props.basketItem.quantity}</h3>
+                                    <button className={styles.basket_button} onClick={props.addToBasket}>{<PlusSmall/>}</button>
                                     </div>
                                 </div>
                         }
