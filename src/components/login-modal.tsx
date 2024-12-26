53e364416ff4a9524c96b247641b087cf8da5df9
Khamadov <121062316+Khamadov@users.noreply.github.com>
2024-12-26 21:32:09 +0300

    Модалка

--- src/components/login-modal.tsx
+++ src/components/login-modal.tsx
@@ -4,6 +4,7 @@ import {useAppDispatch, useAppSelector} from "@/lib/hooks";
 import {SIGN_IN_REQUEST, SIGN_OUT, UPDATE_FOR_API} from "@/lib/reducers";
 import {useEffect, useState} from "react";
 import {useRouter} from "next/navigation";
+import styles from "./login-modal.module.css"
 
 export function Login({onCloseModal = () => {}}) {
     const [login, setLogin] = useState('');
@@ -40,37 +41,37 @@ export function Login({onCloseModal = () => {}}) {
     };
 
     return (
-        <div className="login" onClick={handleOverlayClick}>
-            <div className="login__conteiner">
-                <button className="login__close" onClick={onCloseModal}></button>
+        <div className={styles.login} onClick={handleOverlayClick}>
+            <div className={styles.conteiner}>
+                <button className={styles.close} onClick={onCloseModal}></button>
                 <Image src={"/images/Logo.png"} alt="Логотип" width={251} height={52}/>
-                <form onSubmit={handleSignIn} className="login__form">
-                    <div className="login__content">
-                        <h3 className="login__input_text">Логин</h3>
-                        <input id="name" value={login} onChange={(e) => setLogin(e.target.value)} className={`login__input ${isAuthError && 'auth_error'}`} type="text"/>
+                <form onSubmit={handleSignIn} className={styles.form}>
+                    <div className={styles.content}>
+                        <h3 className={styles.input_text}>Логин</h3>
+                        <input id="name" value={login} onChange={(e) => setLogin(e.target.value)} className={`${styles.input} ${isAuthError && styles.auth_error}`} type="text"/>
                     </div>
-                    <div className="login__content login__content_margin">
-                        <h3 className="login__input_text">Пароль</h3>
-                        <input id="password" value={password} onChange={(e) => setPassword(e.target.value)} className={`login__input ${isAuthError && 'auth_error'}`} type="password"/>
+                    <div className={`${styles.content} ${styles.content_margin}`}>
+                        <h3 className={styles.input_text}>Пароль</h3>
+                        <input id="password" value={password} onChange={(e) => setPassword(e.target.value)} className={`${styles.input} ${isAuthError && styles.auth_error}`} type="password"/>
                     </div>
-                    <div className="login__wrapper">
-                        <button className="login__password">Забыли пароль?</button>
+                    <div className={styles.wrapper}>
+                        <button className={styles.password}>Забыли пароль?</button>
                     </div>
-                    <button className="login__button">Войти</button>
+                    <button className={styles.button}>Войти</button>
                 </form>
-                <div className="login__wrapper login__line_margin">
-                    <div className="login__line login__line_left"></div>
-                    <h3 className="login__line_text">или</h3>
-                    <div className="login__line login__line_right"></div>
+                <div className={`${styles.wrapper} ${styles.line_margin}`}>
+                    <div className={`${styles.line} ${styles.line_left}`}></div>
+                    <h3 className={styles.line_text}>или</h3>
+                    <div className={`${styles.line} ${styles.line_right}`}></div>
                 </div>
-                <div className="login__social">
+                <div className={styles.social}>
                     <Image src={'/images/Yandex_logo.png'} alt="Яндекс" width={57} height={57}/>
                     <Image src={'/images/Mail_logo.png'} alt="Яндекс" width={57} height={57}/>
                     <Image src={'/images/VK_logo_login.png'} alt="Яндекс" width={57} height={57}/>
                 </div>
-                <h3 className="login__registration">
+                <h3 className={styles.registration}>
                     Нет аккаунта? <button onClick={handleNavigationToReg} style={{all: 'unset'}}>
-                        <span className="login__registration_span">Зарегистрироваться</span>
+                        <span className={styles.registration_span}>Зарегистрироваться</span>
                     </button>
                 </h3>
             </div>
