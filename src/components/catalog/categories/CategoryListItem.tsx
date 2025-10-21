7b88cfb4b14e368a6615f6dc626f49a71cf87ba4
Zaur Elbukaev <elbukaevzaur@gmail.com>
2025-10-21 10:58:47 +0300

    fix bug

--- src/components/catalog/categories/CategoryListItem.tsx
+++ src/components/catalog/categories/CategoryListItem.tsx
@@ -34,8 +34,8 @@ export default function CategoryListItem(params: Params) {
                         className="subcatalog__image"
                         src={`${process.env.NEXT_PUBLIC_API_URL}/images/get/product?name=${'small_' + category.defaultImage}`}
                         alt={category.name}
-                        width={296}
-                        height={150}
+                        // width={296}
+                        // height={150}
                         fill
                     />
             }
